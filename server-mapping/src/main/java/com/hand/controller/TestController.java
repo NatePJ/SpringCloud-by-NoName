79bed7480b71f57009ea804cf8035915f1b57896
noname <pj_nate@outlook.com>
2017-08-16 10:47:05 +0800

    3.0

---
+++ server-mapping/src/main/java/com/hand/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.hand.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by NoName on 2017/8/16.
+ */
+@RefreshScope
+@RestController
+class TestController {
+    @Value("${from}")
+    private String from;
+    @RequestMapping("/from")
+    public String from() {
+        return this.from;
+    }
+}
+
